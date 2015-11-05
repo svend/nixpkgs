ffce3c89a3cb14e0ce1768c1b8741faaaada316d
Svend Sorensen <ssorensen@whitepages.com>
2015-11-05 11:27:51 -0800

    Add emacsHEAD

---
+++ pkgs/applications/editors/emacsHEAD/default.nix
@@ -0,0 +1,115 @@
+{ stdenv, fetchurl, ncurses, xlibsWrapper, libXaw, libXpm, Xaw3d
+, pkgconfig, gettext, libXft, dbus, libpng, libjpeg, libungif
+, libtiff, librsvg, texinfo, gconf, libxml2, imagemagick, gnutls
+, alsaLib, cairo, acl, gpm, AppKit
+, withX ? !stdenv.isDarwin
+, withGTK3 ? false, gtk3 ? null
+, withGTK2 ? true, gtk2
+, fetchgit, autoconf, automake
+}:
+
+assert (libXft != null) -> libpng != null;      # probably a bug
+assert stdenv.isDarwin -> libXaw != null;       # fails to link otherwise
+assert withGTK2 -> withX || stdenv.isDarwin;
+assert withGTK3 -> withX || stdenv.isDarwin;
+assert withGTK2 -> !withGTK3 && gtk2 != null;
+assert withGTK3 -> !withGTK2 && gtk3 != null;
+
+let
+  toolkit =
+    if withGTK3 then "gtk3"
+    else if withGTK2 then "gtk2"
+    else "lucid";
+in
+
+stdenv.mkDerivation rec {
+  name = "emacsHEAD";
+
+  builder = ./builder.sh;
+
+  src = fetchgit {
+    url = "http://git.sv.gnu.org/r/emacs.git";
+    rev = "0880d5f6e1ad5006a022416cd21b974c7f5923b9";
+    sha256 = "fe5a7dd38b182f9a39dc748e2f6ab2f1b16aa95ed1833f03e0d76d88d6758cfe";
+  };
+  # src = fetchurl {
+  #   url    = "mirror://gnu/emacs/emacs-24.5.tar.xz";
+  #   sha256 = "0kn3rzm91qiswi0cql89kbv6mqn27rwsyjfb8xmwy9m5s8fxfiyx";
+  # };
+
+  patches = stdenv.lib.optionals stdenv.isDarwin [
+    ./at-fdcwd.patch
+  ];
+
+  postPatch = ''
+    sed -i 's|/usr/share/locale|${gettext}/share/locale|g' lisp/international/mule-cmds.el
+  '';
+
+  buildInputs =
+    [ automake autoconf ncurses gconf libxml2 gnutls alsaLib pkgconfig texinfo acl gpm gettext ]
+    ++ stdenv.lib.optional stdenv.isLinux dbus
+    ++ stdenv.lib.optionals stdenv.isDarwin
+      [ libpng libjpeg libungif libtiff librsvg imagemagick ]
+    ++ stdenv.lib.optionals withX
+      [ xlibsWrapper libXaw Xaw3d libXpm libpng libjpeg libungif libtiff librsvg libXft
+        imagemagick gconf ]
+    ++ stdenv.lib.optional (withX && withGTK2) gtk2
+    ++ stdenv.lib.optional (withX && withGTK3) gtk3
+    ++ stdenv.lib.optional (stdenv.isDarwin && withX) cairo;
+
+  propagatedBuildInputs = stdenv.lib.optional stdenv.isDarwin AppKit;
+
+  preConfigure = ''
+  sed -ie 's|/bin/bash|/bin/sh|g' ./autogen.sh
+  ./autogen.sh
+  '';
+
+  configureFlags =
+    if stdenv.isDarwin
+      then [ "--with-ns" "--disable-ns-self-contained" ]
+    else if withX
+      then [ "--with-x-toolkit=${toolkit}" "--with-xft" ]
+      else [ "--with-x=no" "--with-xpm=no" "--with-jpeg=no" "--with-png=no"
+             "--with-gif=no" "--with-tiff=no" ];
+
+  NIX_CFLAGS_COMPILE = stdenv.lib.optionalString (stdenv.isDarwin && withX)
+    "-I${cairo}/include/cairo";
+
+  postInstall = ''
+    mkdir -p $out/share/emacs/site-lisp/
+    cp ${./site-start.el} $out/share/emacs/site-lisp/site-start.el
+  '' + stdenv.lib.optionalString stdenv.isDarwin ''
+    mkdir -p $out/Applications
+    mv nextstep/Emacs.app $out/Applications
+  '';
+
+  doCheck = false;
+
+  meta = with stdenv.lib; {
+    description = "GNU Emacs 24, the extensible, customizable text editor";
+    homepage    = http://www.gnu.org/software/emacs/;
+    license     = licenses.gpl3Plus;
+    maintainers = with maintainers; [ chaoflow lovek323 simons the-kenny ];
+    platforms   = platforms.all;
+
+    # So that Exuberant ctags is preferred
+    priority = 1;
+
+    longDescription = ''
+      GNU Emacs is an extensible, customizable text editor—and more.  At its
+      core is an interpreter for Emacs Lisp, a dialect of the Lisp
+      programming language with extensions to support text editing.
+
+      The features of GNU Emacs include: content-sensitive editing modes,
+      including syntax coloring, for a wide variety of file types including
+      plain text, source code, and HTML; complete built-in documentation,
+      including a tutorial for new users; full Unicode support for nearly all
+      human languages and their scripts; highly customizable, using Emacs
+      Lisp code or a graphical interface; a large number of extensions that
+      add other functionality, including a project planner, mail and news
+      reader, debugger interface, calendar, and more.  Many of these
+      extensions are distributed with GNU Emacs; others are available
+      separately.
+    '';
+  };
+}
