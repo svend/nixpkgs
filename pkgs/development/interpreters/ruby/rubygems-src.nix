e3d7520f27e66b34417da2be716ac286279304a0
Svend Sorensen <svend@ciffer.net>
2016-09-07 15:29:21 -0700

    Downgrade to rubygems 2.6.4 to fix issue with user-install
    This change can be dropped after 2.6.5 is released.

--- pkgs/development/interpreters/ruby/rubygems-src.nix
+++ pkgs/development/interpreters/ruby/rubygems-src.nix
@@ -1,6 +1,7 @@
+# https://github.com/rubygems/rubygems/pull/1683
 { fetchurl
-, version ? "2.6.6"
-, sha256 ? "0x0ldlwr627d0brw96jdbscib6d2nk19izvnh8lzsasszi1k5rkq"
+, version ? "2.6.4"
+, sha256 ? "1s30l86jkingiv3c2pmfn4jg05439wvkkjg8ah8b64if8r11zmwl"
 }:
 fetchurl {
   url = "http://production.cf.rubygems.org/rubygems/rubygems-${version}.tgz";
