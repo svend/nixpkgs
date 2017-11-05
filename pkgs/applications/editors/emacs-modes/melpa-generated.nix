3c7dd7b862fe5ccde8a375fcf9cd19fb263031ee
Svend Sorensen <svend@svends.net>
2017-11-04 21:00:07 -0700

    Fix missing font-lock-plus

--- pkgs/applications/editors/emacs-modes/melpa-generated.nix
+++ pkgs/applications/editors/emacs-modes/melpa-generated.nix
@@ -25519,7 +25519,26 @@
           license = lib.licenses.free;
         };
       }) {};
-    font-lock-profiler = callPackage ({ emacs, fetchFromGitHub, fetchurl, lib, melpaBuild }:
+    font-lock-plus = callPackage ({ fetchurl, lib, melpaBuild }: melpaBuild {
+        pname = "font-lock-plus";
+        version = "20170222.1755";
+        src = fetchurl {
+          url = "https://www.emacswiki.org/emacs/download/font-lock+.el";
+          sha256 = "0iajkgh0n3pbrwwxx9rmrrwz8dw2m7jsp4mggnhq7zsb20ighs30";
+          name = "font-lock+.el";
+        };
+        recipeFile = fetchurl {
+          url = "https://raw.githubusercontent.com/milkypostman/melpa/a5d15f875b0080b12ce45cf696c581f6bbf061ba/recipes/font-lock+";
+          sha256 = "1wn99cb53ykds87lg9mrlfpalrmjj177nwskrnp9wglyqs65lk4g";
+          name = "font-lock-plus";
+        };
+        packageRequires = [];
+        meta = {
+          homepage = "https://melpa.org/#/font-lock+";
+          license = lib.licenses.free;
+        };
+      }) {};
+  font-lock-profiler = callPackage ({ emacs, fetchFromGitHub, fetchurl, lib, melpaBuild }:
     melpaBuild {
         pname = "font-lock-profiler";
         version = "20170208.1208";
